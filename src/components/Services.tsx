3942e041060c98698b26bfae14569c2198008cad
INTSEAN <119046561+INTSEAN@users.noreply.github.com>
2025-01-03 18:27:43 +0300

    Added title color consistency

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -112,7 +112,7 @@ const Services: React.FC = () => {
           transition={{ duration: 0.5 }}
           className="services-header"
         >
-          <h2>Our Services</h2>
+          <h2 className='font-bold text-4xl'>Our Services</h2>
           <p>Our mission is to empower smallholder farmers and enrich lives through sustainable agribusiness.
           </p>
         </motion.div>
